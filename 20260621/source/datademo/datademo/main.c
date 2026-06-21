c2f58363ece35f5c9e3fd5fb1b5c98778e751865
Administrator <Administrator@DESKTOP-GVTA0LK>
2026-06-22 00:15:39 +0800

    数据存储

---
+++ 20260621/source/datademo/datademo/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <Windows.h>
+
+int main()
+{
+	// 数据类型 变量名 = 初始值;
+
+	// 定义变量
+	BYTE a = 0b00000000;
+
+	//变量赋值
+	a = 0b111111110; // 9个Bit位，截断高位 0x1FE  0xFE
+
+	// 定义变量
+	WORD b = 0b111111110; //WORD 2个字节，16Bit 可以存下 9个Bit  0x1FE
+
+
+	return 0;
+}
